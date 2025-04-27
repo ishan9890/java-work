399ffe0738cbf36b1b6ed37d414e03f992488567
Ishan <ishanjoshi234@gmail.com>
2025-04-27 12:39:06 +0545

    second

--- que1.java
+++ que1.java
@@ -1,5 +1,14 @@
+
+import java.util.Scanner;
+
 public class que1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a word: ");
         
+
+
+
+
     }
 }
