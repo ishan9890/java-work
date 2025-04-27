2fe33400d8c3f56fbff096e2a4bf80e87e0577f6
Ishan <ishanjoshi234@gmail.com>
2025-04-27 12:45:00 +0545

    third

--- que1.java
+++ que1.java
@@ -1,14 +1,17 @@
-
 import java.util.Scanner;
 
 public class que1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a word: ");
-        
-
-
-
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String word = scanner.nextLine();
+        StringBuilder reversedWord = new StringBuilder();
+        int length = word.length();
+        while (length > 0) {
+            reversedWord.append(word.charAt(length - 1));
+            length--;
+        }
+        System.out.println("Reversed word: " + reversedWord.toString());
+        scanner.close();
     }
 }
